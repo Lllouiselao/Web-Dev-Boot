87e09ab335d5f1d6080e423c6076405eeb9f085f
Lllouiselao <louiselao64@gmail.com>
2020-08-20 20:30:40 -0700

    Exercise

---
+++ jQuery/Exercise/exercise.js
@@ -0,0 +1,8 @@
+//give all divs purple background
+$("div").css("background", "purple");
+//give highlight class 200px wide
+$(".highlight").css("width", "200px");
+//give div with id "third"
+$("#third").css("border", "2px solid orange");
+//select first div change font color to pink
+$("div:first-of-type").css("color", "pink");
